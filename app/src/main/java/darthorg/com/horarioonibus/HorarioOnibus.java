ac7db2efdf05ee2853a5ef113fa260cced1a97c5
Wendell Ugalds <wendellugaldsg@gmail.com>
2016-03-11 16:14:47 -0300

    alterados e criados novos botões do menu

--- app/src/main/java/darthorg/com/horarioonibus/HorarioOnibus.java
+++ app/src/main/java/darthorg/com/horarioonibus/HorarioOnibus.java
@@ -1,9 +1,6 @@
 package darthorg.com.horarioonibus;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -81,17 +78,19 @@ public class HorarioOnibus extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_horarios) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_linhas_favoritas) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_temas) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_configuracoes) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_compartilhar) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_sobre) {
+
+        }else if (id == R.id.nav_feedback) {
 
         }
 
